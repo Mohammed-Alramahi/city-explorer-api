a128a214e9a8e8bec1be59c5a2676a6d6a2b2e08
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-05-26 12:31:42 +0300

    refactored server

--- server.js
+++ server.js
@@ -7,37 +7,44 @@ const server=express();
 
 server.use(cors());
 server.listen(port,()=>{
-    console.log("listening to port: "+port,"http://localhost:"+port+"/weather");
+    console.log("listening to port: "+port,"http://localhost:3300/weather?q=amman&lon=36&lat=32");
 });
 server.get('/',(request,response)=>{
- response.statusCode('404').send("not found!")
-});
+ response.send("not found!")
+ });
 server.get('/weather',(request,response)=>{
- let city_name=request.query.searchQuery;
+ let city_name=request.query.q;
  let lon=request.query.lon;
  let lat=request.query.lat;
- let cityData;
  let responseArr=weatherData.find(item=>{
- if(item.city_name.toLowerCase()==city_name.toLowerCase()&&item.lon==lon&&item.lat==lat){
-    cityData=new Forecast(item); 
+ if(item.city_name.toLowerCase()==city_name.toLowerCase()&&Math.round(item.lon)==lon&&Math.round(item.lat)==lat){
+ 
     return item;
      
  }
- 
- console.log(city_name,lon,lat);
  });
- response.send(cityData);
- console.log(cityData);
+ console.log(responseArr);
+ if(responseArr){
+ let cityData=responseArr.data.map(item=>{
+    return new Forecast(item); 
+    });
+    
+        response.send(cityData);
+    }
+    else{
+        response.send("not found!");
+    }
 });
 
 class Forecast{
-    constructor(responseArr){
-            this.data=responseArr.data.map(item=>{
-            let low="Low of "+item.low_temp;
-            let high=", high of "+item.max_temp;
-            return {"description":low+high+" with "+item.weather.description,"date":item.datetime};
-            });
-            
+    constructor(item){
+         this.date=item.datetime;
+         this.description=item.weather.description;
     }
     
 }
+
+server.get('*',(request,response)=>{
+    response.send("not found!")
+    });
+    
