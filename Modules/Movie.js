34c970506ba99752ad0baf8a228b26d5812ad902
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-05-31 20:03:21 +0300

    lab-10 done

--- Modules/Movie.js
+++ Modules/Movie.js
@@ -1,27 +1,32 @@
 const Axios = require('axios');
+let cache={};
 function handleMovieApi(request,response){
     let movieName=request.query.q;
     const envMovieUrl=process.env.MOVIE_API;
     const envMovieApiKey=process.env.MOVIE_API_KEY;
     let movieUrl;
-    if(movieName){
-       movieUrl=`${envMovieUrl}api_key=${envMovieApiKey}&query=${movieName}`
-       console.log(`${envMovieUrl}api_key=${envMovieApiKey}&query=${movieName}`);
+    if (cache[movieName] !== undefined) {
+        console.log('Data came from the Memory');
+        response.send(cache[movieName]);
     }
     else{
-        response.send('Something wrong!');
-    }
-    Axios
-    .get(movieUrl)
-    .then(result=>{
-    let movies=result.data.results.map(item=>{
-        return new Movie(item);
-    })
-    response.send(movies);
-    })
-    .catch(err=>{
-        response.send(err.message);
-    });
+            movieUrl=`${envMovieUrl}api_key=${envMovieApiKey}&query=${movieName}`
+            console.log(`${envMovieUrl}api_key=${envMovieApiKey}&query=${movieName}`);
+         Axios
+         .get(movieUrl)
+         .then(result=>{
+         let movies=result.data.results.map(item=>{
+             return new Movie(item);
+         })
+         cache[movieName] = movies;
+         response.send(movies);
+         })
+         .catch(err=>{
+             response.send(err.message);
+         });
+         console.log("Data came from request");   
+        }
+   
 }
 
 class Movie {
